c62de00f8463e11fb24e40c8a45586813977b573
Brad Voris <40213182+bvoris@users.noreply.github.com>
2020-03-24 06:40:01 -0500

    Update nstb.ps1

--- nstb.ps1
+++ nstb.ps1
@@ -1,10 +1,10 @@
 #Create by Brad Voris
-# writes periods to the screen to prevent screensaver
-# every 120 seconds a period is typed. 
+# writes spaces to the screen to prevent screensaver
+# every 120 seconds a space is typed. 
 
 param($minutes = 120)
 $myshell = New-Object -com "wscript.shell"
 for ($i = 0; $i -lt $minutes; $i++){
 start-sleep -Seconds 120
-$myshell.sendkeys(".")
+$myshell.sendkeys(" ")
 }
